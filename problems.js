315d1ac1a2df6dc35d57f5077718d10ee4b69f56
jyotimasai <ergsjyotimeena0@gmail.com>
2022-12-02 00:24:30 +0530

    some init msg

--- problems.js
+++ problems.js
@@ -6,7 +6,7 @@ for(let i=0;i<=num;i++){
 if(num%i==0){
 count++;
 }
-}if(count==0){
+}if(count==2){
 console.log("It is a prime number");
 }else{
 console.log("It is not prime number");
@@ -17,7 +17,7 @@ Problem 2
 let srt="hello";
 let new_str="";
 for(let i=str.length-1;i>=0;i--){
-new_str=new_str+str[i];
+new_str+=new_str+str[i];
 }if(str==new_str){
 console.log("It is not palindrome");
 }else{
