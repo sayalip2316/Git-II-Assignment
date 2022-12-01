c5b3f37bda4f01324b2df1ac0f371a94bf6c9ed4
SAYALI JADHAV <jadhavsayalip2316@gmail.com>
2022-12-01 20:17:54 +0530

    Prime number and Palindrome

---
+++ problems.js
@@ -0,0 +1,25 @@
+Problem 1
+
+let num=13;
+let count=0;
+for(let i=0;i<=num;i++){
+if(num%i==0){
+count++;
+}
+}if(count==0){
+console.log("It is a prime number");
+}else{
+console.log("It is not prime number");
+}
+
+
+Problem 2
+let srt="hello";
+let new_str="";
+for(let i=str.length-1;i>=0;i--){
+new_str=new_str+str[i];
+}if(str==new_str){
+console.log("It is not palindrome");
+}else{
+console.log("It is palindrome");
+}
